9b4f88d1d36320e360b8a636a64faabb4611ad60
ibadajadeh <rizqullahibad119@gmail.com>
2025-06-10 09:03:08 +0700

    Delete uklku directory

--- uklku/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("config.php");
-$id = $_GET['id']; 
-$result = $conn->prepare("DELETE FROM users WHERE id=?");
-$result->bind_param("i", $id); 
-$result->execute(); 
-$result->close(); 
-header("Location: index.php");
-?>
